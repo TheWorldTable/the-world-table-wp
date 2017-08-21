6a83fda0d3e0afffd8fb30e5741d9fcd2e4ce4e2
Mike Scalora <mike@scalora.org>
2017-08-20 19:25:55 -0700

    New settings API

--- includes/twt-settings.php
+++ includes/twt-settings.php
@@ -11,17 +11,11 @@
 
 <div class="wrap">
   <h2>The World Table Settings</h2>
-  <form action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>" method="post" name="settings_form" id="settings_form">
-    <div class="twt-form-row">
-      <label>SiteId: <input type="text" name="site-id" value="" class="twt-site-id" placeholder="XXXXXXXXXXXXXXXX_XXXXXXX" 
-        <?= empty(get_option('site_id')) ? '' : (' value="' . get_option('site_id') . '" ') ?>
-      /></label>
-    </div>
-    <div class="twt-form-row">
-      Customize other setting at <a href="https://app.worldtable.co/#!/sites">The Workd Table</a> web site. 
-    </div>
-    <div class="twt-action-row">
-      <input type="submit" class="button-primary" value = "Save Changes" name = "save-site-id" />
-    </div>
-  </form>
+    <form action="options.php" method="post">
+      <?php settings_fields('twt-plugin-settings'); ?>
+      <?php do_settings_sections('plugin'); ?>
+      <input name="Submit" type="submit" value="<?php esc_attr_e('Save Settings'); ?>" />
+   </form>
 </div>
+
+<?php
--- the-world-table.php
+++ the-world-table.php
@@ -10,6 +10,29 @@
   Text Domain: the-world-table
 */
 
+function register_plugin_settings() {
+    register_setting( 'twt-plugin-settings', 'twt-plugin-settings', 'plugin_options_validate' );
+    add_settings_section('plugin_main', null, 'twt_settings_form_section_title', 'plugin');
+    add_settings_field('twt-site-id', 'Site Id', 'site_id_form_emit', 'plugin', 'plugin_main');
+}
+
+function twt_settings_form_section_title() {
+  echo '<div class="twt-setting-section-title">Get your World Table Site Id on the <a href="https://app.worldtable.co/#!/sites">profile page</a> </p>';
+}
+
+function site_id_form_emit() {
+  $options = get_option('twt-plugin-settings');
+  echo "<input id='twt-site-id' name='twt-plugin-settings[twt-site-id]' size='40' type='text' value='{$options['twt-site-id']}' />";
+}
+
+function plugin_options_validate($input) {
+  $newinput['twt-site-id'] = trim($input['twt-site-id']);
+  if(!preg_match('/^[a-z0-9]{16}_[a-z0-9]{1,7}$/i', $newinput['twt-site-id'])) {
+    $newinput['twt-site-id'] = '';
+  }
+  return $newinput;
+}
+
 class The_World_Table {
 
     function __construct() {
@@ -23,6 +46,8 @@ class The_World_Table {
     function wpa_add_menu() {
         add_menu_page( 'The World Table', 'World Table', 'manage_options', 'twt-settings',
             array(__CLASS__, 'wpa_page_file_path'), plugins_url('images/logo.png', __FILE__),'2.2.9');
+
+        add_action( 'admin_init', 'register_plugin_settings' );
     }
 
     static function wpa_page_file_path() {
@@ -30,8 +55,12 @@ class The_World_Table {
         include( dirname(__FILE__) . '/includes/twt-settings.php' );
     }
 
-    public function wpa_save_data( $site_id ) {
-        update_option( 'site_id', $site_id );
+    public function wpa_save_data($site_id) {
+        if (get_option('site-id') === false) {
+            add_option('site-id', $site_id, null, 'no');
+        } else {
+            update_option('site-id', $site_id);
+        }
         return true;
     }
 
@@ -40,11 +69,9 @@ class The_World_Table {
     }
 
     function wpa_install() {
-
     }
 
     function wpa_uninstall() {
-
     }
 
 }
